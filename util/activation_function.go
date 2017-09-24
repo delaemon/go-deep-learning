9ae27836d57a698ca2c1d2b5c207f86c66a436fd
delaemon <takeshi.music.5@gmail.com>
2017-09-24 16:13:15 +0900

    Fix scope

--- util/activation_function.go
+++ util/activation_function.go
@@ -2,11 +2,11 @@ package util
 
 import "math"
 
-func sigmoid(x float64) float64 {
+func Sigmoid(x float64) float64 {
 	return 1.0 / (1.0 + math.Pow(math.E, x))
 }
 
-func step(x float64) int {
+func Step(x float64) int {
 	if x >= 0 {
 		return 1
 	}
