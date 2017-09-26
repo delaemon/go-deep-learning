83246d47eaa10e03c1355886377f5af832d7c482
delaemon <takeshi.music.5@gmail.com>
2017-09-26 23:10:22 +0900

    Add Softmax

--- util/activation_function.go
+++ util/activation_function.go
@@ -12,3 +12,26 @@ func Step(x float64) int {
 	}
 	return -1
 }
+
+func Softmax(x []float64, n int) []float64 {
+	y := make([]float64, n)
+	var max float64 = 0
+	var sum float64 = 0
+
+	for i := 0; i < n; i++ {
+		if max < x[i] {
+			max = x[i]
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		y[i] = math.Exp(x[i] - max)
+		sum += y[i]
+	}
+
+	for i := 0; i < n; i++ {
+		y[i] /= sum
+	}
+
+	return y
+}
